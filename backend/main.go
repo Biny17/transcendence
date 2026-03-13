02e512330daaf1c220f6978f5e4470a47ae6d8b4
Tristan <tgallet@student.42.fr>
2026-03-13 17:34:57 +0100

    le debut d'une nouvelle aventure !

---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "context"
+    "net/http"
+
+    "github.com/danielgtaylor/huma/v2"
+    "github.com/danielgtaylor/huma/v2/adapters/humachi"
+    "github.com/go-chi/chi/v5"
+	"fmt"
+)
+
+type Output struct {
+    Body struct {
+        Message string `json:"message"`
+    }
+}
+
+func main() {
+    router := chi.NewMux()
+    api := humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
+
+    huma.Register(api, huma.Operation{
+        OperationID: "get-hello",
+        Method:      http.MethodGet,
+        Path:        "/hello",
+    }, func(ctx context.Context, input *struct{}) (*Output, error) {
+        resp := &Output{}
+        resp.Body.Message = "Hello, world!"
+        return resp, nil
+    })
+	fmt.Println("hello");
+    http.ListenAndServe(":8080", router)
+}
