0bf3cbd6ced573ecd712a96fab981a2e642c650a
Tristan <tgallet@student.42.fr>
2026-03-14 16:51:33 +0100

    ADD: ent

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-    "context"
-    "net/http"
+	"context"
+	"net/http"
 
-    "github.com/danielgtaylor/huma/v2"
-    "github.com/danielgtaylor/huma/v2/adapters/humachi"
-    "github.com/go-chi/chi/v5"
 	"fmt"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/go-chi/chi/v5"
 )
 
 type Output struct {
-    Body struct {
-        Message string `json:"message"`
-    }
+	Body struct {
+		Message string `json:"message"`
+	}
 }
 
 func main() {
-    router := chi.NewMux()
-    api := humachi.New(router, huma.DefaultConfig("API", "1.0.0"))
+	router := chi.NewMux()
+	api := humachi.New(router, huma.DefaultConfig("API", "1.0.0"))
 
-    huma.Register(api, huma.Operation{
-        OperationID: "get-hello",
-        Method:      http.MethodGet,
-        Path:        "/hello",
-    }, func(ctx context.Context, input *struct{}) (*Output, error) {
-        resp := &Output{}
-        resp.Body.Message = "Hello, world!"
-        return resp, nil
-    })
-	fmt.Println("hello world !");
-    http.ListenAndServe(":8080", router)
+	huma.Register(api, huma.Operation{
+		OperationID: "get-hello",
+		Method:      http.MethodGet,
+		Path:        "/hello",
+	}, func(ctx context.Context, input *struct{}) (*Output, error) {
+		resp := &Output{}
+		resp.Body.Message = "Hello, tristan !"
+		return resp, nil
+	})
+	fmt.Println("mdr hello world !")
+	http.ListenAndServe(":8080", router)
 }
