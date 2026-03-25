40881c99a945c62a09cd2d7611ffa4312f654e42
alix <alix.crusoe@gmail.com>
2026-03-25 17:28:07 +0100

    creation dun tableau listant joueurs online

--- front/app/home/page.tsx
+++ front/app/home/page.tsx
@@ -9,6 +9,8 @@ import Chat from '../chat/chat';
 import { ChartComponent } from "../../components/charts/chart"
 import { EngineCanvas } from '@/ThreeWrapper/1.engine/EngineCanvas';
 import { DemoWorld } from '@/ThreeWrapper/2.world/worlds/DemoWorld';
+import ListCard from "@/components/cards/listcard";
+import Online from "@/components/cards/Onlineplayers";
 
 export default function Home() {
   const [signInOpen, setSignInOpen] = useState(false);
@@ -30,42 +32,29 @@ export default function Home() {
       <Background />
       <Navbar signInOpen={signInOpen} setSignInOpen={setSignInOpen} />
 
-      <div className="absolute inset-0 flex items-center justify-center gap-12">
-        <motion.div
-          className="flex flex-col items-center gap-6"
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: signInOpen ? 0 : 1, scale: signInOpen ? 0.8 : 1 }}
-          transition={{ duration: 0.4, ease: "easeOut" }}
-        >
-          {/* <EngineCanvas
-            config={{ mode: 'standalone' , debug:{enabled:true,logPhases:true,showHitboxes:true, showBounds:true, logRenderState:true, logPhaseDurations:true,logPhysicsState:true}}}
-            world={() => new DemoWorld()}
-            style={{ width: '100vw', height: '100vh' }}
-          /> */}
-          <div className="w-[400px] h-[400px] md:w-[600px] md:h-[600px]">
-            <DotLottieReact
-              src="https://lottie.host/4a6712be-c728-4247-a609-65bd86fe8a84/JYUyKJJZH8.lottie"
-              loop
-              autoplay
-            />
-          </div>
-
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: signInOpen ? 0 : 1, y: signInOpen ? 20 : 0 }}
-            transition={{ duration: 0.4, ease: "easeOut" }}
-          >
-            <Button statement="Let's play" />
-          </motion.div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, x: 40 }}
-          animate={{ opacity: signInOpen ? 0 : 1, x: signInOpen ? 40 : 0 }}
-          transition={{ duration: 0.4, ease: "easeOut" }}
-        >
-          {/* <ChartComponent /> */}
-        </motion.div>
+  <div className="absolute inset-0 flex items-center justify-center gap-12">
+  <div className="flex flex-row items-center gap-12">
+    <div className="flex flex-col items-center gap-6">
+      <div className="w-[400px] h-[400px] md:w-[600px] md:h-[600px]">
+        <DotLottieReact
+          src="https://lottie.host/4a6712be-c728-4247-a609-65bd86fe8a84/JYUyKJJZH8.lottie"
+          loop
+          autoplay
+        />
       </div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: signInOpen ? 0 : 1, y: signInOpen ? 20 : 0 }}
+        transition={{ duration: 0.4, ease: "easeOut" }}
+      >
+        <Button statement="Let's play" />
+      </motion.div>
+    </div>
+    <div>
+      <Online />
+    </div>
+  </div>
+</div>
 
       <div className="fixed bottom-4 left-4 z-50 h-[55vh] max-h-128 w-80 max-w-[calc(100vw-2rem)]">
         <Chat />
