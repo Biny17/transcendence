de4726b12cee6e8bf669056d2211a30c1f5ac8af
alix <alix.crusoe@gmail.com>
2026-04-14 17:10:54 +0200

    creation feature orientation guard

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -2,13 +2,16 @@
 import { useState } from 'react';
 import { Navbar } from './Navbar';
 import { Background } from './Background';
+import OrientationGuard  from '../home/Orientation_Guard.jsx';
 
 export default function Home() {
   const [signInOpen, setSignInOpen] = useState(false);
   return (
     <>
+    <OrientationGuard>
       <Background signInOpen={signInOpen} />
       <Navbar signInOpen={signInOpen} setSignInOpen={setSignInOpen} />
+    </OrientationGuard>
     </>
   );
 }
--- front/app/lose/page.tsx
+++ front/app/lose/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import { useState } from 'react';
 import { Background } from './Background';
+import OrientationGuard  from '../home/Orientation_Guard.jsx';
 
 export default function Home() {
   const [signInOpen, setSignInOpen] = useState(false);
   return (
     <>
+    <OrientationGuard>
       <Background />
+    </OrientationGuard>
     </>
   );
 }
